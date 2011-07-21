09772a70fe0755c276a7fe592104c5049922b00d
Julia Yu <juliayu@gmail.com>
2011-07-21 15:29:53 -0400

    fixed carousel infinite padding

--- js/base.js
+++ js/base.js
@@ -2,15 +2,9 @@ $(document).ready(function(){
 	
 	// should probably be calling the app init
 	var setup = function(){
-		// only handles 1 of some carousel
-		$('#kittenheaven').carrotCell();
+
+		$('#kittenheaven').carrotCell({infinite: true, pad: true});
 		$('#kittenhell').carrotCell({sideways: false});
-		// $('#kittenheaven').carrotCell({ infinite: true });
-		// $('#kittenhell').carrotCell({ infinite: true, sideways: false });
-		
-		
-		// var mytest = new test({name : "test 1", scope : $("#kittenheaven")});
-		// var mytest2 = new test({name : "test 2", scope : $("#kittenhell")});
 
 	}
 	
--- js/jCarrotCell.js
+++ js/jCarrotCell.js
@@ -2,11 +2,7 @@
 	
 	var methods = {
 		carrots : {},
-		
-		defaults : {  
-		   	test : true
-		},
-		
+		defaults : {},
 		count : 0,
 		
 		makeCarrot : function(){
@@ -23,9 +19,13 @@
 				rightMost = false,
 				leftMost = true,
 				currentPage = 1,
-				view, slider, items, single, 
+				view, slider, items, single, totalItems,
 				frameSize, singleSize, viewSize,
 				visible, pages, slideBy, prev, next;
+				
+			var repeat = function(str, num) {
+				return new Array( num + 1 ).join( str );
+			}
 
 			/** scroll the carousel
 			*/
@@ -36,33 +36,38 @@
 
 				// after the animation scrolls, set the pages appropriately
 				var scrollHandler = function(){
+					
+					var scrollThis = 0;
+					
 					if (page == 0) {
-						if (settings.sideways) {
-							slider.scrollLeft(singleSize * visible * pages);
+						if (settings.sideways) {					
+							scrollThis = singleSize * visible * pages;
+							view.scrollLeft(scrollThis);	
 						} else {
-							slider.scrollTop(singleSize * visible * pages);
+							view.scrollTop(scrollThis);
 						}
 						page = pages;
+						
 					} else if (page > pages) {
+						var scrollThis = singleSize * visible;
 						if (settings.sideways) {
-							slider.scrollLeft(singleSize * visible);
+							view.scrollLeft(scrollThis);
 						} else {
-							slider.scrollTop(singleSize * visible);
+							view.scrollTop(scrollThis);
 						}
 						page = 1; // reset back to start position
 					}         						                
 					currentPage = page;
+					
 				};
 
 				// set up the animation
 
 				if (settings.sideways) {
-					console.log("scroll sideways");
 					view.filter(':not(:animated)').animate({
 							scrollLeft : '+=' + scrollTo
 					}, 500, scrollHandler);
 				} else {
-					console.log("scroll top");
 					view.filter(':not(:animated)').animate({
 							scrollTop : '+=' + scrollTo
 					}, 500, scrollHandler);
@@ -73,7 +78,6 @@
 			/** move carousel back
 			*/
 			var moveBack = function() {
-				console.log("backward " + settings.name + " " + settings.sideways);
 				if (!settings.infinite && (currentPage == 1)) {
 					return false; // we are at the left most page
 				}
@@ -83,7 +87,6 @@
 			/** move carousel forward
 			*/
 			var moveForward = function() {
-				console.log("forward " + settings.name + " " + settings.sideways);
 				if (!settings.infinite && (currentPage >= pages)) {
 					return false; // we are at the right most page
 				}
@@ -94,13 +97,13 @@
 			*/
 			var setupCarrot = function(){
 
-				
 				view = $this.find(".carrotCellView");
 				slider = view.find('> ol'); // OR ul if !options.ordered
 				items = slider.find('> li'); // OR whatever child element in options
 				single = items.filter(':first');
 				prev = $this.find(".prev"); // OR selector specified in options
 				next = $this.find(".next"); // OR selector specified in options
+				totalItems = items.length;
 
 				if (settings.sideways) {
 					viewSize = $this.innerWidth();
@@ -109,19 +112,23 @@
 					viewSize = $this.innerHeight();
 					singleSize = single.outerHeight(true);
 				}
-
 				visible = Math.floor(viewSize / singleSize);
-				pages = Math.ceil(items.length / visible);
+				pages = Math.ceil(totalItems / visible);
 				slideBy = singleSize * visible;
 
 				if (settings.infinite) {
+					// pad the last page with first items
+					if (totalItems % visible) {
+						var extras = pages*visible - totalItems;
+						items.filter(':last').after(items.slice(0, extras).clone().addClass('extra'));
+						items = slider.find('> li'); // reselect new li
+					}
 					// clone a visible amount on the begin and end
 					items.filter(':first').before(items.slice(-visible).clone().addClass('cloned'));
 					items.filter(':last').after(items.slice(0, visible).clone().addClass('cloned'));
 					items = slider.find('> li'); // reselect new li
 				}
-
-				var slideSize = singleSize * items.length; // find size of all items
+				var slideSize = singleSize * items.length; // find size of all items including cloned
 
 				// set length of slider
 				if (settings.sideways) { 
@@ -131,29 +138,31 @@
 				}
 				view.css("overflow", "hidden"); // clip extra items	
 
+				// move clone items out of site
 				if (settings.infinite) {
-					// move clone items out of site
-					slider.scrollLeft(singleSize * visible); // move cloned items out of sight
+					if (settings.sideways) {
+						view.scrollLeft(singleSize * visible); 
+					} else {
+						view.scrollTop(singleSize * visible); 
+					}
 				}
 				
 				// previous
 				prev.bind("click", function(e){
 					e.preventDefault();
-					console.log(settings.name);
 					moveBack();
 				}).show();
 
 				// next
 				next.bind("click", function(e){
 					e.preventDefault();
-					console.log(settings.name);
 					moveForward();
 				}).show();
 			};
 
 			return {
 				init : function(opt) {
-					$.extend(settings, opt);
+					$.extend(settings, opt); // options over ride settings
 					$this = $(opt.scope);
 					setupCarrot();
 				}
@@ -163,24 +172,18 @@
 		/** initialize jcarousel object
 		*/
 	    init : function( options ) { 	
-			
 			if ( options ) { 
 				 $.extend(options, methods.defaults);
 			}
-	
-			// for each instance of this type of carousel
 			return this.each(function(){
 				methods.count++;
-				
 				var opt = options || {};
 				opt.scope = this;
 				opt.name = $(opt.scope).attr("id") || ("defaultCarrot"+methods.count);
 				methods.carrots[opt.name] = new methods.makeCarrot();
 				methods.carrots[opt.name].init(opt);
 			});
-			
 		}
-		
 	};
 	
 	/** plugin function
